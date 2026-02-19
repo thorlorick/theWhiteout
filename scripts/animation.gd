0020db605d9dd9c8f6652620af2cc1ffc1632b7a
ThorloRick <thorlorick@gmail.com>
2026-02-19 14:37:00 -0500

    Update animation.gd

--- scripts/animation.gd
+++ scripts/animation.gd
@@ -1,7 +1,5 @@
 extends AnimatedSprite2D
 
-@onready connect
-
 func _ready():
     movement.connect("moved_left", play_walk_left)
     movement.connect("moved_right", play_walk_right)
@@ -9,8 +7,4 @@ func _ready():
     movement.connect("moved_down", play_walk_down)
 	
 
-func update_animation():
-	if body.velocity != Vector2.ZERO:
-		sprite.play("walk")
-	else:
-		sprite.play("idle")
+
