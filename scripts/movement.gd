37b179f465f0c4a0034e63821b17b1e93d7954af
ThorloRick <thorlorick@gmail.com>
2026-02-19 14:51:45 -0500

    Update movement.gd

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -1,23 +1,21 @@
 extends Node
 
-var body: CharacterBody2D
-var speed_component
+signal walk_left
+signal walk_right
+signal walk_down
+signal walk_up
 
-func _ready():
-	body = get_parent()
-	speed_component = body.get_node("Speed")
+func onready ()
 
-func physics_update():
-	var direction = Vector2.ZERO
+if Input.is_action_pressed("walk_left"):
+    emit_signal("walk_left")
 
-	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
+if Input.is_action_pressed("walk_right"):
+    emit_signal("walk_right")
 
-	body.velocity = direction.normalized() * speed_component.value
-	body.move_and_slide()
+if Input.is_action_pressed("walk_down"):
+    emit_signal("walk_down")
+
+if Input.is_action_pressed("walk_up"):
+    emit_signal("walk_up")
+
