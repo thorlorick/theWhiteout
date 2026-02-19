16f6570e0f595f601023c53a59d86c3327b99c40
ThorloRick <thorlorick@gmail.com>
2026-02-19 14:57:41 -0500

    Update movement.gd

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -5,17 +5,17 @@ signal walk_right
 signal walk_down
 signal walk_up
 
-func onready ()
+func _process (delta):
 
-if Input.is_action_pressed("walk_left"):
-    emit_signal("walk_left")
+    if Input.is_action_pressed("walk_left"):
+        emit_signal("walk_left")
 
-if Input.is_action_pressed("walk_right"):
-    emit_signal("walk_right")
+    if Input.is_action_pressed("walk_right"):
+        emit_signal("walk_right")
 
-if Input.is_action_pressed("walk_down"):
-    emit_signal("walk_down")
+    if Input.is_action_pressed("walk_down"):
+        emit_signal("walk_down")
 
-if Input.is_action_pressed("walk_up"):
-    emit_signal("walk_up")
+    if Input.is_action_pressed("walk_up"):
+        emit_signal("walk_up")
 
