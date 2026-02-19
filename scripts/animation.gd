369ec5b510ec6fdeab61c7f7ab532a5b141d333b
ThorloRick <thorlorick@gmail.com>
2026-02-19 14:47:47 -0500

    Update animation.gd

--- scripts/animation.gd
+++ scripts/animation.gd
@@ -7,6 +7,18 @@ func _ready():
     movement.connect("moved_right", play_walk_right)
     movement.connect("moved_up", play_walk_up)
     movement.connect("moved_down", play_walk_down)
-	
+
+
+func play_walk_left():
+    play("walk_left")
+
+func play_walk_right():
+    play("walk_right")
+
+func play_walk_up():
+    play("walk_up")
+
+func play_walk_down():
+    play("walk_down")
 
 
