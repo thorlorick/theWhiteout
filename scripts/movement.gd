80b1483dea66320ed405d09eb8fbd786b636ef35
ThorloRick <thorlorick@gmail.com>
2026-02-22 15:14:54 -0500

    Update movement.gd

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -19,7 +19,7 @@ func _process(delta):
 
 extends Node
 
-signal direction_input(direction)
+signal direction_input(direction: Vector2)
 
 func _process(delta):
     if Input.is_action_pressed("walk_left"):
