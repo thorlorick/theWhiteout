5fc5f74eb631285541a577c08ad12883c8d8b069
ThorloRick <thorlorick@gmail.com>
2026-02-22 15:29:26 -0500

    Update movement.gd

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -1,24 +1,5 @@
 extends Node
 
-signal walk_left
-signal walk_right
-signal walk_down
-signal walk_up
-
-func _process(delta):
-	if Input.is_action_pressed("walk_left"):
-		walk_left.emit()
-	elif Input.is_action_pressed("walk_right"):
-		walk_right.emit()
-	elif Input.is_action_pressed("walk_down"):
-		walk_down.emit()
-	elif Input.is_action_pressed("walk_up"):
-		walk_up.emit()
-
-# movement with one direction signal that says which way it's going.
-
-extends Node
-
 signal direction_input(direction: Vector2)
 
 func _process(delta):
