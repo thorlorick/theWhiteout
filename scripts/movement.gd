1c282ad676ecb4199501f2b01baa599f01237822
ThorloRick <thorlorick@gmail.com>
2026-02-23 12:07:00 -0500

    Update movement.gd

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -2,6 +2,9 @@ extends Node
 
 signal direction_input(direction: Vector2)
 
+var current_direction: Vector2 = Vector2.ZERO
+var _last_direction: Vector2 = Vector2.ZERO
+
 func _process(delta):
 	var direction = Vector2.ZERO
 	
@@ -14,4 +17,9 @@ func _process(delta):
 	if Input.is_action_pressed("walk_down"):
 		direction.y += 1
 	
-	direction_input.emit(direction.normalized())
+    direction = direction.normalized()
+    
+    if direction != _last_direction:
+        _last_direction = direction
+        current_direction = direction
+        direction_input.emit(direction)
