75a01a7762a1ebf5209be6a571a75f4fa1cc032f
ThorloRick <thorlorick@gmail.com>
2026-02-22 19:53:40 -0500

    Update movement.gd

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -3,15 +3,15 @@ extends Node
 signal direction_input(direction: Vector2)
 
 func _process(delta):
-    var direction = Vector2.ZERO
-    
-    if Input.is_action_pressed("walk_left"):
-        direction.x -= 1
-    if Input.is_action_pressed("walk_right"):
-        direction.x += 1
-    if Input.is_action_pressed("walk_up"):
-        direction.y -= 1
-    if Input.is_action_pressed("walk_down"):
-        direction.y += 1
-    
-    direction_input.emit(direction)
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("walk_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("walk_right"):
+		direction.x += 1
+	if Input.is_action_pressed("walk_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("walk_down"):
+		direction.y += 1
+	
+	direction_input.emit(direction.normalized())
