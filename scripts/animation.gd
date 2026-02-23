c6e0fecf6834127412da78033efca1952dab29c7
ThorloRick <thorlorick@gmail.com>
2026-02-23 12:55:41 -0500

    Update animation.gd

--- scripts/animation.gd
+++ scripts/animation.gd
@@ -1,26 +1,39 @@
 extends Sprite2D
 
-var last_direction: String = "down"
+var _last_direction: String = "down"
+var _is_moving: bool = false
+var _is_attacking: bool = false
+
+func _ready():
+    $AnimationPlayer.animation_finished.connect(_on_animation_finished)
 
 func _on_direction_input(direction: Vector2):
-	if direction == Vector2.ZERO:
-		$AnimationPlayer.play("idle_" + last_direction)
-		return
-	
-	if abs(direction.x) > abs(direction.y):
-		if direction.x < 0:
-			last_direction = "left"
-			$AnimationPlayer.play("walk_left")
-		else:
-			last_direction = "right"
-			$AnimationPlayer.play("walk_right")
-	else:
-		if direction.y < 0:
-			last_direction = "up"
-			$AnimationPlayer.play("walk_up")
-		else:
-			last_direction = "down"
-			$AnimationPlayer.play("walk_down")
-			
+    _is_moving = direction != Vector2.ZERO
+    
+    if direction != Vector2.ZERO:
+        if abs(direction.x) > abs(direction.y):
+            _last_direction = "left" if direction.x < 0 else "right"
+        else:
+            _last_direction = "up" if direction.y < 0 else "down"
+    
+    _update_animation()
+
 func _on_attack_input():
-	$AnimationPlayer.play("attack_" + last_direction)
+    if not _is_attacking:
+        _is_attacking = true
+        _update_animation()
+
+func _on_animation_finished(_anim_name: String):
+    if _is_attacking:
+        _is_attacking = false
+        _update_animation()
+
+func _update_animation():
+    if _is_attacking and _is_moving:
+        $AnimationPlayer.play("walk_attack_" + _last_direction)
+    elif _is_attacking:
+        $AnimationPlayer.play("attack_" + _last_direction)
+    elif _is_moving:
+        $AnimationPlayer.play("walk_" + _last_direction)
+    else:
+        $AnimationPlayer.play("idle_" + _last_direction)
