0c8782564cf66a5a657632915c8952e6a3d295bc
ThorloRick <thorlorick@gmail.com>
2026-02-22 19:38:02 -0500

    Update animation.gd

--- scripts/animation.gd
+++ scripts/animation.gd
@@ -1,20 +1,27 @@
 extends Sprite2D
 
-func _ready():
-    var movement = get_parent().get_node("Movement")
-    movement.direction_input.connect(_on_direction_input)
+var last_direction: String = "down"
 
 func _on_direction_input(direction: Vector2):
     if direction == Vector2.ZERO:
+        $AnimationPlayer.play("idle_" + last_direction)
         return
     
     if abs(direction.x) > abs(direction.y):
         if direction.x < 0:
+            last_direction = "left"
             $AnimationPlayer.play("walk_left")
         else:
+            last_direction = "right"
             $AnimationPlayer.play("walk_right")
     else:
         if direction.y < 0:
+            last_direction = "up"
             $AnimationPlayer.play("walk_up")
         else:
+            last_direction = "down"
             $AnimationPlayer.play("walk_down")
+
+func _ready():
+    var movement = get_parent().get_node("Movement")
+    movement.direction_input.connect(_on_direction_input)
