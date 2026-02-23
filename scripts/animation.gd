4f87c5dbca568c36fd984282f7c1c1eef0a958e3
ThorloRick <thorlorick@gmail.com>
2026-02-22 19:26:43 -0500

    Update animation.gd

--- scripts/animation.gd
+++ scripts/animation.gd
@@ -1,18 +1,20 @@
 extends Node
 
 func _ready():
-    # We need to go UP to the parent (Player_1) and then DOWN to Movement Component
     var movement = get_parent().get_node("Movement")
-    
-    # Connect its signal to our function below
     movement.direction_input.connect(_on_direction_input)
 
-func _on_direction_input(direction: String):
-    if direction == "left":
-        $AnimationPlayer.play("walk_left")
-    elif direction == "right":
-        $AnimationPlayer.play("walk_right")
-    elif direction == "up":
-        $AnimationPlayer.play("walk_up")
-    elif direction == "down":
-        $AnimationPlayer.play("walk_down")
+func _on_direction_input(direction: Vector2):
+    if direction == Vector2.ZERO:
+        return
+    
+    if abs(direction.x) > abs(direction.y):
+        if direction.x < 0:
+            $AnimationPlayer.play("walk_left")
+        else:
+            $AnimationPlayer.play("walk_right")
+    else:
+        if direction.y < 0:
+            $AnimationPlayer.play("walk_up")
+        else:
+            $AnimationPlayer.play("walk_down")
