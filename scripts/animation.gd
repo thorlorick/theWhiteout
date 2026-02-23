458411f5a726e2b8aac5b389eb0932cfbbcd6433
ThorloRick <thorlorick@gmail.com>
2026-02-22 21:16:10 -0500

    Update animation.gd

--- scripts/animation.gd
+++ scripts/animation.gd
@@ -3,27 +3,30 @@ extends Sprite2D
 var last_direction: String = "down"
 
 func _on_direction_input(direction: Vector2):
-    if direction == Vector2.ZERO:
-        $AnimationPlayer.play("idle_" + last_direction)
-        return
-    
-    if abs(direction.x) > abs(direction.y):
-        if direction.x < 0:
-            last_direction = "left"
-            $AnimationPlayer.play("walk_left")
-        else:
-            last_direction = "right"
-            $AnimationPlayer.play("walk_right")
-    else:
-        if direction.y < 0:
-            last_direction = "up"
-            $AnimationPlayer.play("walk_up")
-        else:
-            last_direction = "down"
-            $AnimationPlayer.play("walk_down")
+	if direction == Vector2.ZERO:
+		$AnimationPlayer.play("idle_" + last_direction)
+		return
+	
+	if abs(direction.x) > abs(direction.y):
+		if direction.x < 0:
+			last_direction = "left"
+			$AnimationPlayer.play("walk_left")
+		else:
+			last_direction = "right"
+			$AnimationPlayer.play("walk_right")
+	else:
+		if direction.y < 0:
+			last_direction = "up"
+			$AnimationPlayer.play("walk_up")
+		else:
+			last_direction = "down"
+			$AnimationPlayer.play("walk_down")
+			
+func _on_attack_input():
+	$AnimationPlayer.play("attack_" + last_direction)
 
 func _ready():
-    var movement = get_parent().get_node("Movement")
-    movement.direction_input.connect(_on_direction_input)
-    var combat = get_parent().get_node("Combat")
-    combat.attack_input.connect(_on_attack_input)
+	var movement = get_parent().get_node("Movement")
+	movement.direction_input.connect(_on_direction_input)
+	var combat = get_parent().get_node("Combat")
+	combat.attack_input.connect(_on_attack_input)
