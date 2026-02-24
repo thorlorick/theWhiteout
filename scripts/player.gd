876d935af18a4c1e6d3b9d9b57ee9f8750d913f4
ThorloRick <thorlorick@gmail.com>
2026-02-23 21:06:45 -0500

    Update player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,11 +3,11 @@ extends CharacterBody2D
 @onready var stats = $Stats
 @onready var movement = $Movement
 @onready var combat = $Combat
-@onready var animation = $Animation
+@onready var animation_player = $Animation
 
 func _ready():
-    movement.direction_input.connect(sprite._on_direction_input)
-    combat.attack_input.connect(animation._on_attack_input)
+    movement.direction_input.connect(animation_player._on_direction_input)
+    combat.attack_input.connect(animation_player._on_attack_input)
 
 func _physics_process(_delta):
     velocity = movement.current_direction * stats.speed
