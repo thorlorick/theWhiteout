d0a470b47fb3d0314d5d45bad76844e0dd05972f
ThorloRick <thorlorick@gmail.com>
2026-02-25 13:42:16 -0500

    Update enemy_ai.gd

--- scripts/enemy_ai.gd
+++ scripts/enemy_ai.gd
@@ -1,3 +1,19 @@
 extends node
 
-enum: 
+@onready var detection_zone = $DetectionZone
+
+enum State { IDLE, PATROL, CHASE, ATTACK }
+
+var current_state = State.IDLE
+
+func _physics_process(delta):
+    match current_state:
+        State.IDLE:
+            handle_idle()
+        State.PATROL:
+            handle_patrol()
+        State.CHASE:
+            handle_chase()
+        State.ATTACK:
+            handle_attack()
+
