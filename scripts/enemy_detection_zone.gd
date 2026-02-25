1ec37df7efb0a444cb4b8edca55bd0243d04ca26
ThorloRick <thorlorick@gmail.com>
2026-02-25 14:34:45 -0500

    Update enemy_detection_zone.gd

--- scripts/enemy_detection_zone.gd
+++ scripts/enemy_detection_zone.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-signal body_entered_zone(body)
-signal body_exited_zone(body)
+signal body_entered_detection_zone(body)
+signal body_exited_detection_zone(body)
 
 @onready var detection_zone = $DetectionZone
 
@@ -10,7 +10,7 @@ func _ready():
     detection_zone.body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body):
-    body_entered_zone.emit(body)
+    body_entered_detection_zone.emit(body)
 
 func _on_body_exited(body):
-    body_exited_zone.emit(body)
+    body_exited_detection_zone.emit(body)
