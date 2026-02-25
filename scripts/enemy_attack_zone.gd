b142709c2316f044bd58578d8d8396b71cd378e7
ThorloRick <thorlorick@gmail.com>
2026-02-25 14:34:20 -0500

    Update enemy_attack_zone.gd

--- scripts/enemy_attack_zone.gd
+++ scripts/enemy_attack_zone.gd
@@ -1,1 +1,16 @@
-hold for code
+extends Node2D
+
+signal body_entered_attack_zone(body)
+signal body_exited_attack_zone(body)
+
+@onready var attack_zone = $AttackZone
+
+func _ready():
+    attack_zone.body_entered.connect(_on_body_entered)
+    attack_zone.body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body):
+    body_entered_attack_zone.emit(body)
+
+func _on_body_exited(body):
+    body_exited_attack_zone.emit(body)
