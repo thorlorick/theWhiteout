8893aead5243c0273431703e1346418f00d6ec13
ThorloRick <thorlorick@gmail.com>
2026-02-25 14:48:54 -0500

    Update enemy_ai.gd

--- scripts/enemy_ai.gd
+++ scripts/enemy_ai.gd
@@ -1,18 +1,7 @@
 extends Node2D
 
 enum State { IDLE, PATROL, CHASE, ATTACK }
+
 var current_state = State.IDLE
-
-@onready var detection_zone = $DetectionZone
-
-func _physics_process(delta):
-    match current_state:
-        State.IDLE:
-            handle_idle()
-        State.PATROL:
-            handle_patrol()
-        State.CHASE:
-            handle_chase()
-        State.ATTACK:
-            handle_attack()
+var target = null
 
