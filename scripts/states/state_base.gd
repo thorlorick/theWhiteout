916b24b590859b6d4b1f5d53aaec6ef820a55cb5
ThorloRick <thorlorick@gmail.com>
2026-02-26 11:32:02 -0500

    Update state_base.gd

--- scripts/states/state_base.gd
+++ scripts/states/state_base.gd
@@ -1,13 +1,19 @@
+# state_base.gd
 class_name StateBase
 extends Node2D
 
 signal transition_requested(state_name: String)
 
-func enter() -> void:
-    pass
+var entity = null
+
+func enter(owner) -> void:
+    entity = owner
 
 func exit() -> void:
     pass
 
 func update(_delta: float) -> void:
+    pass
+
+func handle_input(_event: InputEvent) -> void:
     pass
