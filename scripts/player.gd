d246829d74edd676052ef0efe4837f6ee9c071f6
ThorloRick <thorlorick@gmail.com>
2026-02-26 13:13:05 -0500

    Update player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,17 @@
+# hero.gd
 extends CharacterBody2D
 
-@onready var stats = $Stats
+@onready var fsm: FSM = $FSM
+@onready var health = $Health
 @onready var movement = $Movement
+@onready var animation = $Animation
 @onready var combat = $Combat
-@onready var animation_player = $Animation
 
-func _ready():
-    movement.direction_input.connect(animation_player._on_direction_input)
-    combat.attack_input.connect(animation_player._on_attack_input)
+func _ready() -> void:
+    fsm.init(self)
 
-func _physics_process(_delta):
-    velocity = movement.current_direction * stats.speed
-    move_and_slide()
+func _physics_process(delta: float) -> void:
+    fsm.update(delta)
+
+func _input(event: InputEvent) -> void:
+    fsm.handle_input(event)
