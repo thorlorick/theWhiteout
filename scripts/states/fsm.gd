a8d1ef8cf10f42e24f727659a3140745c5a7e9e5
ThorloRick <thorlorick@gmail.com>
2026-02-26 12:59:10 -0500

    Update fsm.gd

--- scripts/states/fsm.gd
+++ scripts/states/fsm.gd
@@ -1,34 +1,36 @@
 # fsm.gd
 class_name FSM
-extends Node2D
+extends Node
 
-var current_state: Node2D = null
+var current_state: FSMStateBase = null
 var entity = null
+var states := {}
 
 func init(owner) -> void:
     entity = owner
     for child in get_children():
-        if child.has_signal("transition_requested"):
+        if child is FSMStateBase:
+            states[child.state_name] = child
             child.transition_requested.connect(_on_transition_requested)
-    if get_children().size() > 0:
-        _enter_state(get_children()[0])
+    if states.has("Idle"):
+        _enter_state(states["Idle"])
+    elif states.size() > 0:
+        _enter_state(states.values()[0])
 
 func update(delta: float) -> void:
-    if current_state != null:
+    if current_state:
         current_state.update(delta)
 
 func handle_input(event: InputEvent) -> void:
-    if current_state != null:
+    if current_state:
         current_state.handle_input(event)
 
-func _enter_state(new_state: Node2D) -> void:
-    if current_state != null:
+func _enter_state(new_state: FSMStateBase) -> void:
+    if current_state:
         current_state.exit()
     current_state = new_state
     current_state.enter(entity)
 
 func _on_transition_requested(state_name: String) -> void:
-    for child in get_children():
-        if child.name == state_name:
-            _enter_state(child)
-            return
+    if states.has(state_name):
+        _enter_state(states[state_name])
