479895a5c901e579b023f60d3673acffaac96ab8
ThorloRick <thorlorick@gmail.com>
2026-02-26 14:32:19 -0500

    Update enemy_movement.gd

--- scripts/enemy_movement.gd
+++ scripts/enemy_movement.gd
@@ -1,1 +1,10 @@
 # enemy autonomous movement
+# enemy_movement.gd
+
+extends Node
+
+var speed: float = 100.0
+var entity: CharacterBody2D = null
+
+func setup(entity_owner: CharacterBody2D) -> void:
+    entity = entity_owner
