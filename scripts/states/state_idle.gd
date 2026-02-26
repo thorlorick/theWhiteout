3daba2e4138499bce9b81f7700231438f3c2ed8b
ThorloRick <thorlorick@gmail.com>
2026-02-26 13:06:12 -0500

    Create state_idle.gd

---
+++ scripts/states/state_idle.gd
@@ -0,0 +1,14 @@
+# state_idle.gd
+extends FSMStateBase
+
+var idle_timer: float = 0.0
+const IDLE_DURATION: float = 2.0
+
+func enter(owner) -> void:
+    super.enter(owner)
+    idle_timer = 0.0
+
+func update(delta: float) -> void:
+    idle_timer += delta
+    if idle_timer >= IDLE_DURATION:
+        transition_requested.emit("Patrol")
