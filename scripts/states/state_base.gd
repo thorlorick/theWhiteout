c9462cecd724b7e1422f27a2aa89ef8aa1244cec
ThorloRick <thorlorick@gmail.com>
2026-02-26 10:29:27 -0500

    Create state_base.gd

---
+++ scripts/states/state_base.gd
@@ -0,0 +1,13 @@
+class_name StateBase
+extends Node2D
+
+signal transition_requested(state_name: String)
+
+func enter() -> void:
+    pass
+
+func exit() -> void:
+    pass
+
+func update(_delta: float) -> void:
+    pass
