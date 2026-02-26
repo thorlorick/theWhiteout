527bcbb72ec61520f681ec15a63616f1daa8998c
ThorloRick <thorlorick@gmail.com>
2026-02-26 11:30:10 -0500

    Update fsm.gd

--- scripts/states/fsm.gd
+++ scripts/states/fsm.gd
@@ -1,25 +1,31 @@
+# fsm.gd
 class_name FSM
 extends Node2D
 
 var current_state: Node2D = null
+var entity = null
 
-func _ready() -> void:
+func init(owner) -> void:
+    entity = owner
     for child in get_children():
         if child.has_signal("transition_requested"):
             child.transition_requested.connect(_on_transition_requested)
-    
     if get_children().size() > 0:
         _enter_state(get_children()[0])
 
-func _process(delta: float) -> void:
+func update(delta: float) -> void:
     if current_state != null:
         current_state.update(delta)
+
+func handle_input(event: InputEvent) -> void:
+    if current_state != null:
+        current_state.handle_input(event)
 
 func _enter_state(new_state: Node2D) -> void:
     if current_state != null:
         current_state.exit()
     current_state = new_state
-    current_state.enter()
+    current_state.enter(entity)
 
 func _on_transition_requested(state_name: String) -> void:
     for child in get_children():
