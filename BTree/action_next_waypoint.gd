2ea6da10d54751b09e569c1a2c8a3f13f41071e4
ThorloRick <thorlorick@gmail.com>
2026-03-02 19:53:35 -0500

    Update action_next_waypoint.gd

--- BTree/action_next_waypoint.gd
+++ BTree/action_next_waypoint.gd
@@ -5,8 +5,7 @@ extends BTNode
 func tick(actor, blackboard) -> Status:
     var waypoint = blackboard["current_waypoint"]
     
-    if actor.position.distance_to(waypoint) > 5:
-        actor.move_toward(waypoint)
+
         return Status.RUNNING
     
     return Status.SUCCESS
