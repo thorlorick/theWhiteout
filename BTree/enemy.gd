56977e1b0472f1c33e84421d7fac68b2eb18578f
ThorloRick <thorlorick@gmail.com>
2026-03-04 11:21:36 -0500

    Update enemy.gd

--- BTree/enemy.gd
+++ BTree/enemy.gd
@@ -1,45 +1,54 @@
-# enemy.gd
 extends CharacterBody2D
 
+var home_position: Vector2
+
 var blackboard = {
-    "target_position": Vector2.ZERO,
-    "idle_timer": 0,
-    "current_waypoint_index": 0,
-    "waypoints": [],
-    "home_position": Vector2.ZERO
+	"target_position": Vector2.ZERO,
+	"idle_timer": 0,
+	"current_waypoint_index": 0,
+	"current_waypoint": Vector2.ZERO,
+	"waypoints": [],
+	"home_position": Vector2.ZERO
 }
 
 var tree
 
 func _ready():
-    home_position = global_position
-    blackboard["home_position"] = global_position
-    blackboard["waypoints"] = [
-        $Waypoint1.position,
-        $Waypoint2.position,
-        $Waypoint3.position
-    ]
-    tree = _build_tree()
+	print("Waypoint1 node: ", $"../Waypoint1")
+	home_position = global_position
+	blackboard["home_position"] = global_position
+	blackboard["waypoints"] = [
+		$"../Waypoint1".global_position,
+		$"../Waypoint2".global_position,
+		$"../Waypoint3".global_position
+	]
+	blackboard["current_waypoint"] = blackboard["waypoints"][0]
+	tree = _build_tree()
+
+func move_toward(target: Vector2):
+	var direction = (target - global_position).normalized()
+	velocity = direction * 100
+	move_and_slide()
 
 func _build_tree():
-    var root = BTSelector.new()
-    
-    var patrol_sequence = BTSequence.new()
-    patrol_sequence.children = [
-        ConditionIsPatrolTimerReady.new(),
-        ActionPatrol.new(),
-        ActionNextWaypoint.new(),
-        ActionReturnHome.new(),
-        ConditionIsAtHome.new(),
-        ActionResetTimer.new()
-    ]
-    
-    root.children = [
-        patrol_sequence,
-        ActionIdle.new()
-    ]
-    
-    return root
+	var root = BTSelector.new()
+	
+	var patrol_sequence = BTSequence.new()
+	patrol_sequence.children = [
+		ConditionIsPatrolTimerReady.new(),
+		ActionPatrol.new(),
+		ActionNextWaypoint.new(),
+		ActionReturnHome.new(),
+		ConditionIsAtHome.new(),
+		ActionResetTimer.new()
+	]
+	
+	root.children = [
+		patrol_sequence,
+		ActionIdle.new()
+	]
+	
+	return root
 
 func _process(delta):
-    tree.tick(self, blackboard)
+	tree.tick(self, blackboard)
