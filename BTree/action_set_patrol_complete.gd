92b3e7ec272b89e7e40283da5bb68de5b7e04325
ThorloRick <thorlorick@gmail.com>
2026-03-04 17:14:19 -0500

    Delete BTree/action_set_patrol_complete.gd

--- BTree/action_set_patrol_complete.gd
+++
@@ -1,7 +0,0 @@
-# action_set_patrol_complete.gd
-class_name ActionSetPatrolComplete
-extends BTNode
-func tick(actor, blackboard) -> Status:
-    blackboard["patrol_complete"] = true
-    return Status.SUCCESS
-
