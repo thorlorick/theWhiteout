d3148dbd02563ec2c97b8b43cc502862092bb3ac
ThorloRick <thorlorick@gmail.com>
2026-03-05 07:09:07 -0500

    Update action_patrol.gd

--- BTree/action_patrol.gd
+++ BTree/action_patrol.gd
@@ -3,10 +3,9 @@ class_name ActionPatrol
 extends BTNode
 
 func tick(actor, blackboard) -> Status:
-    var waypoint = blackboard["current_waypoint"]
-    
-    if actor.position.distance_to(waypoint) > 5:
-        actor.move_toward(waypoint)
-        return Status.RUNNING
-    
-    return Status.SUCCESS
+	var waypoint = blackboard["current_waypoint"]
+	print("ActionPatrol ticking - position: ", actor.position, " target: ", waypoint, " distance: ", actor.position.distance_to(waypoint))
+	if actor.position.distance_to(waypoint) > 5:
+		actor.move_toward(waypoint)
+		return Status.RUNNING
+	return Status.SUCCESS
