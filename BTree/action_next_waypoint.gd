7741c7ce3f7b492a28d070411dff766a1b56d312
ThorloRick <thorlorick@gmail.com>
2026-03-05 07:08:42 -0500

    Update action_next_waypoint.gd

--- BTree/action_next_waypoint.gd
+++ BTree/action_next_waypoint.gd
@@ -2,10 +2,12 @@
 class_name ActionNextWaypoint
 extends BTNode
 func tick(actor, blackboard) -> Status:
-    var index = blackboard["current_waypoint_index"]
-    var total = blackboard["waypoints"].size()
-    blackboard["current_waypoint_index"] = (index + 1) % total
-    blackboard["current_waypoint"] = blackboard["waypoints"][blackboard["current_waypoint_index"]]
-    if blackboard["current_waypoint_index"] == 0:
-        blackboard["patrol_complete"] = true
-    return Status.SUCCESS
+	var index = blackboard["current_waypoint_index"]
+	var total = blackboard["waypoints"].size()
+	blackboard["current_waypoint_index"] = (index + 1) % total
+	blackboard["current_waypoint"] = blackboard["waypoints"][blackboard["current_waypoint_index"]]
+	if blackboard["current_waypoint_index"] == 0:
+		blackboard["patrol_complete"] = true
+		print("Patrol complete!")
+	print("ActionNextWaypoint - moving to index: ", blackboard["current_waypoint_index"], " position: ", blackboard["current_waypoint"])
+	return Status.SUCC
