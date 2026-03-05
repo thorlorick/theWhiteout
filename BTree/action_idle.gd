5e32b895a7c5697954038faee02f74a0776a94d4
ThorloRick <thorlorick@gmail.com>
2026-03-05 07:08:21 -0500

    Update action_idle.gd

--- BTree/action_idle.gd
+++ BTree/action_idle.gd
@@ -3,5 +3,6 @@ class_name ActionIdle
 extends BTNode
 
 func tick(actor, blackboard) -> Status:
-    blackboard["idle_timer"] += 1
-    return Status.SUCCESS
+	print("ActionIdle ticking - idle_timer: ", blackboard["idle_timer"])
+	blackboard["idle_timer"] += 1
+	return Status.SUCCESS
