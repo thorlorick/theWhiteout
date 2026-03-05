e18067790c39fced7c9321e44c911a9059d5e7cf
ThorloRick <thorlorick@gmail.com>
2026-03-05 07:09:32 -0500

    Update action_return_home.gd

--- BTree/action_return_home.gd
+++ BTree/action_return_home.gd
@@ -1,10 +1,11 @@
 # action_return_home.gd
 class_name ActionReturnHome
 extends BTNode
-
 func tick(actor, blackboard) -> Status:
-    if actor.position.distance_to(blackboard["home_position"]) > 20:
-        actor.move_toward(blackboard["home_position"])
-        return Status.RUNNING
-    
-    return Status.SUCCESS
+	if actor.position.distance_to(blackboard["home_position"]) > 15:
+		actor.move_toward(blackboard["home_position"])
+		return Status.RUNNING
+	
+	blackboard["patrol_complete"] = false
+	blackboard["idle_timer"] = 0
+	return Status.SUCCESS
