5639c451307912c6b4345ab3809fd545e7ea11f4
ThorloRick <thorlorick@gmail.com>
2026-03-07 12:49:20 -0500

    Create goals_component.gd

---
+++ GOAP/Components/goals_component.gd
@@ -0,0 +1,14 @@
+class_name GoalsComponent
+
+var goals = [
+	{
+		"name": "BeHome",
+		"desired_state": {"at_home": true},
+		"priority": 0.0
+	},
+	{
+		"name": "Patrol",
+		"desired_state": {"patrolling": true},
+		"priority": 0.0
+	}
+]
