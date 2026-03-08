b10b03fa645889904adbbf9583154ebcfac7ca04
ThorloRick <thorlorick@gmail.com>
2026-03-07 21:58:34 -0500

    Update goals_component.gd

--- GOAP/Components/goals_component.gd
+++ GOAP/Components/goals_component.gd
@@ -10,5 +10,10 @@ var goals = [
 		"name": "Patrol",
 		"desired_state": {"patrolling": true},
 		"priority": 0.0
-	}
+	},
+	{
+		"name": "CloseTheGap",             # NEW: replaces "Chase"
+		"desired_state": {"gap_closed": true},  # resolved when UE is caught or lost
+		"priority": 0.0
+	},
 ]
