d915d94cdfac3b91f233e99753b622f2acbf1fb2
ThorloRick <thorlorick@gmail.com>
2026-03-09 07:07:51 -0400

    Update actions_component.gd

--- GOAP/Components/actions_component.gd
+++ GOAP/Components/actions_component.gd
@@ -2,21 +2,22 @@ class_name ActionsComponent
 
 var actions = [
 	{
-		"name": "GoHome",
+		"name":          "GoHome",
 		"preconditions": {"at_home": false},
-		"effects":       {"at_home": true, "patrolling": false},
+		"effects":       {"at_home": true},
 		"cost": 1.0
 	},
 	{
-		"name": "GoPatrol",
+		"name":          "GoPatrol",
 		"preconditions": {},
-		"effects":       {"patrolling": true, "at_home": false},
+		"effects":       {"at_home": false},
 		"cost": 1.0
 	},
 	{
-		"name": "ChaseUE",
-		"preconditions": {"sees_ue": true},     # needs to see the UE to chase
-		"effects":       {"gap_closed": true},   # NEW: chasing resolves the gap goal
+		"name":          "ChaseUE",
+		"preconditions": {"sees_ue": true},
+		"effects":       {"gap_closed": true},
 		"cost": 1.0
 	},
 ]
+
