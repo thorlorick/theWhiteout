8da7863da8ba977ba4fc04353d7d1eef73cf0d47
ThorloRick <thorlorick@gmail.com>
2026-03-09 07:11:33 -0400

    Update vision_component.gd

--- GOAP/Components/vision_component.gd
+++ GOAP/Components/vision_component.gd
@@ -33,7 +33,6 @@ func _cast_rays() -> void:
 		var result = space_state.intersect_ray(query)
 		if result:
 			if result.collider.is_in_group("ue"):
-				print(">>> UE SPOTTED BY GROUP!")
 				spotted_ue.emit(result.collider)
 				return
 
