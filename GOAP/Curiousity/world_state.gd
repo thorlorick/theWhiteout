c8589a978d16ce5da3329212a12c8c127724c14e
ThorloRick <thorlorick@gmail.com>
2026-03-10 19:38:58 -0400

    Create world_state.gd

---
+++ GOAP/Curiousity/world_state.gd
@@ -0,0 +1,9 @@
+var _state: Dictionary = {
+	"at_home":      false,
+	"patrolling":   false,
+	"sees_ue":      false,
+	"ue_target":    null,
+	"gap_closed":   false,
+	"target_lost":  false,
+	"target_found": true,
+}
