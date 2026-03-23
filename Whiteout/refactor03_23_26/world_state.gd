0a3ef0dc11e95ccfd73441bdd08c345f1b9febb6
ThorloRick <thorlorick@gmail.com>
2026-03-23 19:32:21 -0400

    Create world_state.gd

---
+++ Whiteout/refactor03_23_26/world_state.gd
@@ -0,0 +1,24 @@
+class_name WorldState
+
+# -----------------------------------------------------------------------------
+# WorldState
+# Simple key/value store understanding of the world.
+# Single source of truth. Everything reads from here, nothing else.
+# -----------------------------------------------------------------------------
+
+var _state: Dictionary = {
+	"at_home":       false,
+	"patrolling":    false,
+	"sees_target":       false,
+	"known_target":     null,
+	"gap_closed":    false,
+	"target_lost":   false,
+	"target_found":  true,
+	"target_eliminated": false,
+}
+
+func get_state(key: String) -> Variant:
+	return _state.get(key, null)
+
+func set_state(key: String, value: Variant) -> void:
+	_state[key] = value
