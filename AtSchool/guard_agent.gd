4b51375494166d8228a52cd632be634d1c106931
ThorloRick <thorlorick@gmail.com>
2026-04-09 10:31:06 -0400

    Update guard_agent.gd

--- AtSchool/guard_agent.gd
+++ AtSchool/guard_agent.gd
@@ -325,7 +325,6 @@ func _on_spotted_target(target_body: Node2D) -> void:
 	_last_known_position  = target_body.global_position
 	_last_known_direction = (target_body.global_position - global_position).normalized()
 	urge.on_target_spotted()
-	combat_meter.lock_target(target_body)
 	_replan()
 
 func _on_confirmed_target_lost() -> void:
