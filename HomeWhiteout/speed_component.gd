2ac69b0a7192ec8c23da4fbf871b5a8fc86828b4
ThorloRick <thorlorick@gmail.com>
2026-04-10 20:35:51 -0400

    Add chase_speed variable and getter method
    Added a new variable for chase speed and a method to retrieve it.

--- HomeWhiteout/speed_component.gd
+++ HomeWhiteout/speed_component.gd
@@ -2,10 +2,14 @@ class_name SpeedComponent
 extends Node
 
 @export var base_speed: float = 20.0
+@export var chase_speed: float = 30.0
 @export var run_speed: float = 40.0
 
 func get_speed() -> float:
 	return base_speed
+
+func get_chase_speed() -> float:
+	return chase_speed
 
 func get_run_speed() -> float:
 	return run_speed
